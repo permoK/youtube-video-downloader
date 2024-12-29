07d661d2765ce447c23252fb34cde7f00ee6d078
permoK <perminusk21@gmail.com>
2024-12-29 16:50:16 +0300

    port

--- app.py
+++ app.py
@@ -53,4 +53,4 @@ def home():
     return render_template('index.html', message=message)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5200, debug=True)
